647c19a5675f5655e6c3f06d2266fbee415ce64d
Mayank singh <mayank23378@gmail.com>
2024-03-11 06:20:14 +0530

    adding js file to weather

---
+++ Weather/weather.js
@@ -0,0 +1,9 @@
+const apiKey = "d3c42713f099a12710261a32d83aefb3";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bihar";
+
+async function checkWeather() {
+    const respons = await fetch(apiUrl + `&appid=${apiKey}`);
+    var data = await respons.jeson();
+
+    console.log(data);
+}
