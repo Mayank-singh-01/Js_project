35c68b97a7fc91cabd6b79493e22c170df4aefd3
Mayank singh <mayank23378@gmail.com>
2024-03-15 23:27:53 +0530

    done with footer section

---
+++ E-commerce/eCom.js
@@ -0,0 +1,17 @@
+<div class=" self-center py-12 text-gray-200 gap-1 text-xl">
+                <p class="py-2"> 
+                    <i class="fa-solid fa-envelope fa-xl" style="color: #db1450;"></i>Mayank23378@gmail.com
+                </p>
+                <p class="py-2">
+                    <i class="fa-solid fa-phone fa-xl" style="color: #db143c;"></i>+91 8271694644
+                </p>
+                <p class="py-2">
+                    <i class="fa-solid fa-location-dot fa-xl" style="color: #dc0450;"></i>  Gaya,Bihar
+                </p>
+               <div class="gap-1 flex flex-col">
+                <a class="py-2" href="https://www.linkedin.com/in/mayank-singh-a76577266/" target="_blank"><i class="fa-brands fa-linkedin fa-xl" style="color: #d60a47;"></i>in/mayank-singh-a</a>
+                <a class="py-2" href="https://twitter.com/__mayank_singh_" target="_blank"><i class="fa-brands fa-square-twitter fa-xl" style="color: #d9124d;"></i>@__mayank_singh_</a>
+                <a class="py-2" href="https://www.instagram.com/mayank_singh__18/" target="_blank"><i class="fa-brands fa-square-instagram fa-xl" style="color: #d10a3c;"></i>mayank_singh__18</a>
+                <a class="py-2" href="https://github.com/m-k-singh1" target="_blank"><i class="fa-brands fa-square-github fa-xl" style="color: #cf074d;"></i>github.com/m-k-singh1</a>
+               </div>
+            </div>
