d4656ed4851aa4476263daaf18dd21b13170c052
Mayank singh <mayank23378@gmail.com>
2024-03-31 14:58:34 +0530

    done with html menuplation

--- Weather/weather.js
+++ Weather/weather.js
@@ -1,10 +1,21 @@
+
 const apiKey = "d3c42713f099a12710261a32d83aefb3";
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=bihar";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather() {
-    const respons = await fetch(apiUrl + `&appid=${apiKey}`);
-    var data = await respons.jeson();
-
-    console.log(data);
-    
+async function checkWeather(location) {
+    try {
+        const response = await fetch(apiUrl + location + `&appid=${apiKey}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        document.querySelector(".temp").innerHTML = `${Math.round(data.main.temp)}°C`;
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".humidity").innerHTML = `${data.main.humidity}%`;
+        document.querySelector(".wind").innerHTML = `${data.wind.speed}km/h`;
+    } catch (error) {
+        console.error("Error occur", error);
+    }
 }
+
+checkWeather("gaya");
