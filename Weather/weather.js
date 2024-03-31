421241fd236d24733a2ce9553d1b5d30431af9be
Mayank singh <mayank23378@gmail.com>
2024-03-31 16:10:00 +0530

    weather project is done

--- Weather/weather.js
+++ Weather/weather.js
@@ -3,6 +3,7 @@ const apiKey = "d3c42713f099a12710261a32d83aefb3";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const search = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const image = document.querySelector(".weather-icon");
 
 async function checkWeather(location) {
     try {
@@ -15,6 +16,25 @@ async function checkWeather(location) {
         document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".humidity").innerHTML = `${data.main.humidity}%`;
         document.querySelector(".wind").innerHTML = `${data.wind.speed}km/h`;
+        let imgIcon = data.weather[0].main;
+        if (imgIcon === "Clouds") {
+            image.src = "../image/clouds.png";
+        } 
+        else if (imgIcon === "Clear") {
+            image.src = "../image/clear.png";
+        }
+        else if (imgIcon === "Rain") {
+            image.src = "../image/rain.png";
+        }
+        else if (imgIcon === "Drizzle") {
+            image.src = "../image/drizzle.png";
+        }
+        else if (imgIcon === "Mist") {
+            image.src = "../image/mist.png";
+        }
+        else if (imgIcon === "Wind") {
+            image.src = "../image/wind.png";
+        }
     } catch (error) {
         console.error("Error occur", error);
     }
